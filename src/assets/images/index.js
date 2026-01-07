fc4ff21ff0574d5181d9f7c109493d4fa5c619ea
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2026-01-07 09:23:27 +0530

    Replace PNG home images with WebP format
    Swapped LandBG, LandLeft, and LandRight images from PNG to WebP for improved performance and updated imports in index.js accordingly.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,9 @@
 // Import and Export the Files using Index.js is Represent a Best Coder. 
 import TranslateIcon from "./icons/translate.png"
 
-import LandBG from "./home/LandBG.png"
-import LandLeft from "./home/LandLeft.png"
-import LandRight from "./home/LandRight.png"
+import LandBG from "./home/LandBG.webp"
+import LandLeft from "./home/LandLeft.webp"
+import LandRight from "./home/LandRight.webp"
 
 export default {
     TranslateIcon,
